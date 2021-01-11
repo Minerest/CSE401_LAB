1ec5a58f8433e59b51b68e062b00f26d78eaa64c
jacobvilla645 <77129539+jacobvilla645@users.noreply.github.com>
2021-01-10 18:33:38 -0800

    Update Branches.asm

--- lab03/Branches.asm
+++ lab03/Branches.asm
@@ -2,7 +2,7 @@
 .text
 
 main:
-	lw $s0, A			# Loading A, B, C and Z into registers (integer assignments)
+	lw $s0, A		# Loading A, B, C and Z into registers (integer assignments)
 	lw $s1, B
 	lw $s2, C
 	lw $s3, Z
@@ -12,22 +12,22 @@ main:
 	
 	add $t0, $s2, 1		# ( C + 1 ), saved to be utilized later 
 	bgt $s0, $s1, AND	# if ( A > B ) go to AND, a second check for statement to jump to L2.
-	j L3				# if we dont jump to AND, 'and' statement is incorrect, and thus we jump to L3
+	j L3			# if we dont jump to AND, 'and' statement is incorrect, and thus we jump to L3
 
 AND:
 	beq $t0, 7, L2		# Second check for and Statement
-	j L3 				# and if we dont jump, again we jump to L3
+	j L3 			# and if we dont jump, again we jump to L3
 	
-L1: add $s3, $s3, -1	# Load '-1' into register $s3
-	sw $s3, Z			# Store register $s3 in Z
-	j Exit				# get out 
+L1: 	add $s3, $s3, -1	# Load '-1' into register $s3
+	sw $s3, Z		# Store register $s3 in Z
+	j Exit			# get out 
 	
-L2: add $s3, $s3, -2	# Load '-2' into register $s3
-	sw $s3, Z			# Store register $s3 in Z
-	j Exit				# get out
+L2: 	add $s3, $s3, -2	# Load '-2' into register $s3
+	sw $s3, Z		# Store register $s3 in Z
+	j Exit			# get out
 	
-L3: add $s3, $s3, 0		# Load '-1' into register $s3
-	sw $s3, Z			# Store register $s3 in Z
+L3: 	add $s3, $s3, 0		# Load '0' into register $s3
+	sw $s3, Z		# Store register $s3 in Z
 	
 Exit:
 	li $v0 10
